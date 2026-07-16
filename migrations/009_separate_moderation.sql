9ffcd1d0e3c1135ad6bc8d14598fa289cc1409dd
DeusFlow <deuswork@icloud.com>
2026-07-16 14:59:20 +0200

    chore: update migration scripts to use idempotent column additions and remove destructive down migrations

--- migrations/009_separate_moderation.sql
+++ migrations/009_separate_moderation.sql
@@ -1,7 +1,3 @@
 -- +migrate Up
-ALTER TABLE articles ADD COLUMN posted_threads BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE articles ADD COLUMN tg_message_id INTEGER NOT NULL DEFAULT 0;
-
--- +migrate Down
-ALTER TABLE articles DROP COLUMN posted_threads;
-ALTER TABLE articles DROP COLUMN tg_message_id;
+ALTER TABLE articles ADD COLUMN IF NOT EXISTS posted_threads BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE articles ADD COLUMN IF NOT EXISTS tg_message_id INTEGER NOT NULL DEFAULT 0;
--- migrations/010_threads_body.sql
+++ migrations/010_threads_body.sql
@@ -1,5 +1,2 @@
 -- +migrate Up
 ALTER TABLE articles ADD COLUMN IF NOT EXISTS body_threads TEXT NOT NULL DEFAULT '';
-
--- +migrate Down
-ALTER TABLE articles DROP COLUMN IF EXISTS body_threads;
